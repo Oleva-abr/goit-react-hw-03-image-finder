34253a31c8c37c94e48ab01720328a5e61f112c0
Olena Abramova <olena.v.abramova@gmail.com>
2021-04-06 23:37:01 +0300

    start modal

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import React, { Component } from 'react';
+import Modal from './components/Modal';
 import './App.css';
 
-const App = () => (
-  <>
-    <Modal />
-  </>
-);
+class App extends Component {
+  state = {
+    showModal: false,
+  };
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({
+      showModal: !showModal,
+    }));
+  };
+  render() {
+    return <>{showModal && <Modal onClose={this.toggleModal} />}</>;
+  }
+}
 
 export default App;
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,10 +1,28 @@
-const Modal = () => (
-  <>
-    <div className="Overlay">
-      <div className="Modal">
-        <img src="" alt="" />
-      </div>
-    </div>
-  </>
-);
+import React, { Component } from 'react';
+import { createPortal } from 'react-dom';
+
+const modalRoot = document.querySelector('#modal-root');
+class Modal extends Component {
+  componentDidMount() {
+    window.addEventListener('keydown', e => {
+      if (e.code === 'Escape') {
+        this.props.onClose();
+      }
+    });
+      this.componentWillUnmount(){
+          
+    }
+  }
+  render() {
+    return createPortal(
+      <div className="Overlay">
+        <div className="Modal">
+          {this.props.children}
+          <img src="" alt="" />
+        </div>
+      </div>,
+      modalRoot,
+    );
+  }
+}
 export default Modal;
